df962a9ae09ecaa5f3fdc82319de5dfccc58d557
Bean Warring <brianthetall@gmail.com>
2012-02-25 03:11:24 -0500

    The misc functions file is at a stable point.

--- functions.c
+++ functions.c
@@ -10,88 +10,68 @@ In order to use the math library, must compile with "-lm" flag
 #define DEBUG 1
 #define LINE 256
 #define ARGS 1
-#define VECTOR 6
-void printFunction(const char*);
-int computeFloatingPointVector(long double*,const long double*,const long double*);//use powl
-char* mirrorString(const char *);
-int pointerLength(const void*);//return the number of bytes between the pointer & the '\0'
+#define VECTOR 3
+void printFunction(const char*);//TESTED
+int computeFloatingPointVector(double*,const double*,const double*);//TESTED
+char* mirrorString(const char *,char*);//TESTED
+int pointerLength(const void*);//return the number of bytes between the pointer & the '\0'. Good for char strings.
 int main(int argc, char *argv[])
 {
-  char *s;
+  char *s,*t;
   int i;
-  long double *bases,*exponents,*result;
+  double *bases,*exponents,*result;
   if(argc!=ARGS)
     {
       printf("functions");
       return -1;
     }
-  bases=(long double*)calloc(VECTOR,sizeof(long double));
-  exponents=(long double*)calloc(VECTOR,sizeof(long double));
-  result=(long double*)calloc(VECTOR,sizeof(long double));
-  for(i=0;i<5;i++)
-    {
-      bases[i]=i+9;
-      exponents[i]=i+8*2;
-    }
-  bases[5]='\0';
-  exponents[5]='\0';//function that finds size needs null-terminated pointers
-  printf("basesLength=%d\r\n",pointerLength(bases));
-  printf("computeFloatingPointVector=%d\r\n",computeFloatingPointVector(result,bases,exponents));
+  printFunction("Output from testing the printFunction\r\n");//testing the printFunction()
+  bases=(double*)calloc(VECTOR,sizeof(double));//declaring memory for the vectors
+  exponents=(double*)calloc(VECTOR,sizeof(double));//exponent vector
+  result=(double*)calloc(VECTOR,sizeof(double));//result vector
+  bases[0]=16.3;exponents[0]=4.3;//initalize some date
+  bases[1]=136.3;exponents[1]=44.3;//initalize some date
+  printf("computeFloatingPointVector=%d\r\n",computeFloatingPointVector(result,bases,exponents));//TEST FloatingVector function
   for(i=0;i<VECTOR;i++)
     {
       printf("result[%d]=%f\r\n",i,(float)result[i]);
     }
   s=(char *)calloc(LINE,sizeof(char));
+  t=(char *)calloc(LINE,sizeof(char));
   s="this is a line.";
-  printf("%s\r\n%s\r\n",s,mirrorString(s));
+  printf("%s\r\n%s\r\n",s,mirrorString(s,t));
   printf("pointerLength(\"this is a line.\")=%d\r\n",pointerLength(s));
 
-  //free(result);
-  //free(exponents);
-  //free(bases);
   return 0;
 }
 //-------MISC Function Examples-------------------------------------
 void printFunction(const char* s)
 {
   if(s!=NULL)
-    {
       printf("%s",s);
-    }
 }
-int computeFloatingPointVector(long double* retval,const long double* a,const long double* b)
+int computeFloatingPointVector(double* retval,const double* a,const double* b)
 {//use powl
-  //  long double* retval;
   int i;
-  printf("IN");
   if(a==NULL || b==NULL)
       return -1;
-  if(pointerLength(a)!=pointerLength(b))  //make sure vectors are the same length
-      return -2;
-  printf("PointnerLengthA=%d\r\n",pointerLength(a));
-  for(i=0;i<pointerLength(a);i++)
+  for(i=0;i<VECTOR;i++)
     {
-      retval[i]=powl(a[i],b[i]);
-      printf("%f^%f=%f\r\n",(double)a[i],(double)b[i],(double)retval[i]);
+      retval[i]=powl(a[i],b[i]);//compute the operation a^b
+      printf("%.2f^%.2f=%.2f\r\n",(double)a[i],(double)b[i],(double)retval[i]);//print all involved
     }
-  printf("leaving");
-
   return 0;
 }
-char* mirrorString(const char *in)
-{
-  char *s;
+char* mirrorString(const char *in,char *out)
+{//mirror *in. store results in *out
   int i,inLength=strlen(in);
-  s=(char*)calloc(inLength,sizeof(char));
   for(i=0;i<inLength;i++)
-    {
-      s[i]=in[inLength-1-i];
-    }
-  return s;
+    out[i]=in[inLength-1-i];
+  return out;
 }
 int pointerLength(const void* v)//return the number of bytes between the pointer & the '\0'
 {
   int i=0;
-  while(((char*)v)[i++]!='\0')
+  while(((char*)v)[i++]!='\0'){printf(".");}
   return i;
 }
