e1cc155333a030e0a396c3c0d2e0e5a76a05327c
Brian Wagner <brianthetall@gmail.com>
2012-06-17 12:49:42 -0600

    interesting is pre-working.

--- interesting.c
+++ interesting.c
@@ -1,13 +1,32 @@
 //compile with gcc -lm interesting.c
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
-#define ARGS 4
+#define ARGS 5
 #define DEBUG 1
 
 int main(int argc,char *argv[]){
+  float pile,pileChange,fluxRate,minimum,total;//min. can be rate X% OR amount $X
+  int compoundPeriod,iteration=0;
   if(argc!=ARGS){
     printf("interesting <pile> <fluxion-rate> <compound-period-months> <minimum-shovel-rate/amount>\r\n");
+    return -1;
   }
+  if(DEBUG){
+    printf("%s\t%s\t%s\t%s\t%s\r\n",argv[0],argv[1],argv[2],argv[3],argv[4]);
+  }
+  pile=(float)atof(argv[1]);
+  fluxRate=(float)atof(argv[2]);
+  compoundPeriod=atoi(argv[3]);
+  minimum=(float)atof(argv[4]);
+  if(DEBUG){
+    printf("%f\t%f\t%d\t%f\r\n",pile,fluxRate,compoundPeriod,minimum);
+  }
+  do{
+    printf("%d) Pile=%f\t",iteration++,pile);
+    pileChange=(pile-minimum)*fluxRate;
+    printf("NewPile=%f\tRxed:%f\r\n",pile-=pileChange,total+=minimum);
+  }while(pileChange > 0.69);
   return 0;
 }
