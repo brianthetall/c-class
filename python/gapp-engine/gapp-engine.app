4fc05deaa02a08ff4be105c24c9bde0bf10e1204
brian wagner <brian@brianthetall.com>
2013-02-03 13:30:17 -0700

    isolated html files from python script that serves them

--- python/gapp-engine/gapp-engine.app
+++
@@ -1,9 +0,0 @@
-import webapp2
-
-class MainPage(webapp2.RequestHandler):
-      def get(self):
-      	  self.response.headers['Content-Type'] = 'text/plain'
-	  self.response.out.write('this documentation sucks ass')
-
-app = webapp2.WSGIApplication([('/', MainPage),
-                              debug=True)
--- python/webAppMultiPage/multipage.py
+++ python/webAppMultiPage/multipage.py
@@ -1,14 +1,25 @@
 import webapp2
-
+#import os
+#import index.html
 class MainPage(webapp2.RequestHandler):
   def get(self):
-      self.response.headers['Content-Type'] = 'text/plain'
-      self.response.out.write('Hello, MultiPage World!')
+      self.response.headers['Content-Type'] = 'html'#'text/plain'
+#      self.response.out.write(os.path())
+#read in HTML file and write it.....
+      index=open('index.html')
+#      for line in index.input():
+      self.response.out.write(index.read())
 
 class SecondPage(webapp2.RequestHandler):
   def get(self):
-    self.response.headers['Content-Type'] = 'text/plain'
-    self.response.write('Second Page!')
+    self.response.headers['Content-Type'] = 'html'
+    self.response.write("""
+<html><head>
+<title>Welcome In!</title></head>
+<body>You made it past the login
+</body>
+</html>
+""")
 
 app = webapp2.WSGIApplication([('/', MainPage),('/second',SecondPage)],
                               debug=True)
